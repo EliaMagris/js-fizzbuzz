a683fd2f991037fd6708ef6d0232ba24a8326b50
EliaMagris <elia.magris@libero.it>
2022-10-21 16:17:15 +0200

    1° bonus

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,12 @@
+let NumberId = document.getElementById("Container");
+
+let element = document.createElement("div");
+
+element.classList.add('text-black',"fs-4", "p-5");
+
+NumberId.classList.add("bg-warning")
+
+NumberId.append(element);
 
 
 
@@ -5,14 +14,20 @@ for (let i = 1; i <= 100; i++) {
 
     if (i % 3 == 0 && i % 5 == 0){
         console.log(`${i} FIZZBUZZ`);
+        element.innerHTML += `<p>${i} FIZZBUZZ</p>`;
     
     }else if(i % 3 == 0){
-       console.log(`${i} FIZZ`);
+        console.log(`${i} FIZZ`);
+        element.innerHTML += `<p>${i} FIZZ</p>`;
     
     }else if (i % 5 == 0){
-       console.log(`${i} BUZZ`);
+        console.log(`${i} BUZZ`);
+        element.innerHTML += `<p">${i} BUZZ</p>`;
+        element.classList.add("bg.warning")
     
     }else{
         console.log(`${i}`);
+        element.innerHTML += `<p>${i}</p>`;
     }
 } 
+
