351a0cb71f531ebc433a205a4257c4c99fd49ed7
EliaMagris <elia.magris@libero.it>
2022-10-22 14:26:12 +0200

    esercizio corretto

--- assets/main.js
+++ assets/main.js
@@ -1,31 +1,33 @@
-let NumberId = document.getElementById("Container");
-
-let element = document.createElement("div");
-
-element.classList.add('text-white',"fs-4", "p-5", "bg-danger");
-
-NumberId.classList.add("bg-warning")
-
-NumberId.append(element);
 
 
 for (let i = 1; i <= 100; i++) {
 
+    let NumberId = document.getElementById("numberList");
+
+    let element = document.createElement("li");
+
     if (i % 3 == 0 && i % 5 == 0){
         console.log(`${i} FIZZBUZZ`);
-        element.innerHTML += `<p class= "bg-warning text-center text-black">${i} FIZZBUZZ</p>`;
+        element.innerHTML += `${i} FIZZBUZZ`;
+        element.style.backgroundColor = "blue"
+        element.style.color = "white"
     
     }else if(i % 3 == 0){
         console.log(`${i} FIZZ`);
-        element.innerHTML += `<p class= "bg-secondary text-black">${i} FIZZ</p>`;
+        element.innerHTML += `${i} FIZZ`;
+        element.style.backgroundColor = "red"
     
     }else if (i % 5 == 0){
         console.log(`${i} BUZZ`);
-        element.innerHTML += `<p class= "bg-primary text-black">${i} BUZZ</p>`;
+        element.innerHTML += `${i} BUZZ`;
+        element.style.backgroundColor = "lightblue"
     
     }else{
         console.log(`${i}`);
-        element.innerHTML += `<p class= "bg-info text-black">${i}</p>`;
+        element.innerHTML += `${i}`;
+        element.style.backgroundColor = "yellow"
     }
+
+    NumberId.append(element);
 } 
 
